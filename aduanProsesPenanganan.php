237381828bd2de689c0fe59f898019bd0a6a6049
haniffauzandaroini-create <haniffauzandaroini@uin-malang.ac.id>
2026-08-20 16:05:48 +0700

    Add files via upload

---
+++ aduanProsesPenanganan.php
@@ -0,0 +1,310 @@
+<?php include( "contentsConUser.php" );
+  $username = $_SESSION['username'];
+  $myquery = "SELECT * FROM dt_all_user WHERE username='$username'";
+  $d = mysqli_query($con, $myquery)or die( mysqli_error($con));
+  $dtUser = mysqli_fetch_assoc($d);
+  $idAdm = $dtUser['username'];
+  ?>
+<!DOCTYPE html>
+<html lang="en">
+  <?php include( "headUser.php" );?> 
+  <body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper" style="min-height: 223.667px;">
+      <?php 
+        include( "navtopUser.php" );
+        include( "navAdmTa.php" );
+        ?> 
+      <div class="content-wrapper">
+        <div class="content-header">
+          <div class="container-fluid">
+            <?php include( "alertUser.php" );?>
+            <div class="row mb-2">
+              <div class="col-sm-6">
+                <h4 class="mb-0">Aduan Perbaikan</h4>
+              </div>
+              <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                  <li class="breadcrumb-item active">Proses Penanganan</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+        <?php include 'pagination.php';
+          if(isset($_REQUEST['keyword']) && $_REQUEST['keyword']<>""){
+          $keyword=$_REQUEST['keyword'];
+          $reload = "aduanProsesPenanganan.php?pagination=true&keyword=$keyword";
+          
+          $sql = "SELECT 
+          r.id
+          , r.kode_tiket
+          , r.tgl_pengajuan
+          , r.level_pengajuan
+          , r.nm_pengaju
+          , r.lokasi_kampus
+          , r.lokasi_gedung
+          , r.lokasi_ruang
+          , r.aitem_kerusakan
+          , r.tingkat_kerusakan
+          , r.img_kerusakan
+          , r.deskripsi_kerusakan
+          , r.admin
+          , r.kat_koordinator
+          , r.nm_koordinator
+          , r.tgl_penugasan
+          , r.tgl_pra_penanganan
+          , r.tgl_proses_penanganan
+          , r.tgl_selesai_penanganan
+          , r.status_tiket
+          , r.durasi_penanganan
+          , r.approval
+          , r.tgl_approval
+          , r.validasi_admin
+          , r.tgl_validasi_admin
+          , r.catatan
+          , olp.nm
+          , dau.username
+          , dau_admin.username
+          , dau_koord.username
+          , olk.nm_kampus
+          , olg.nm_gedung
+          , oak.nm
+          , otk.nm
+          , ost.nm
+          , oap.nm
+          , ov.nm
+
+          FROM tiket_masuk r
+          
+          LEFT JOIN opsi_level_pengajuan olp
+          on r.level_pengajuan = olp.id
+          LEFT JOIN dt_all_user dau
+          on r.nm_pengaju = dau.username
+          LEFT JOIN dt_all_user dau_admin
+          on r.admin = dau_admin.username
+          LEFT JOIN dt_all_user dau_koord
+          on r.nm_koordinator = dau_koord.username
+          LEFT JOIN opsi_lokasi_kampus olk
+          on r.lokasi_kampus = olk.id_kampus
+          LEFT JOIN opsi_lokasi_gedung olg
+          on r.lokasi_gedung = olg.id_gedung
+          LEFT JOIN opsi_lokasi_ruang olr
+          on r.lokasi_ruang = olr.id_ruang
+          LEFT JOIN opsi_aitem_kerusakan oak
+          on r.aitem_kerusakan = oak.id
+          LEFT JOIN opsi_tingkat_kerusakan otk
+          on r.tingkat_kerusakan = otk.id
+          LEFT JOIN opsi_status_tiket ost
+          on r.status_tiket = ost.id
+          LEFT JOIN opsi_approval oap
+          on r.approval = oap.id
+          LEFT JOIN opsi_validasi ov
+          on r.validasi_admin = ov.id
+          LEFT JOIN dt_pelaksanaan_tiket dpTiket
+          on r.id = dpTiket.id_tiket
+          LEFT JOIN dt_all_user dau_pelaksana
+          on dpTiket.id_pelaksana = dau_pelaksana.username
+          
+          WHERE (r.id LIKE '%$keyword%' OR r.kode_tiket LIKE '%$keyword%' OR r.tgl_pengajuan LIKE '%$keyword%' OR r.tgl_penugasan LIKE '%$keyword%' OR r.tgl_pra_penanganan LIKE '%$keyword%' OR r.tgl_proses_penanganan LIKE '%$keyword%' OR r.tgl_selesai_penanganan LIKE '%$keyword%' OR r.deskripsi_kerusakan LIKE '%$keyword%' OR dau_admin.username LIKE '%$keyword%' OR dau_admin.nm_person LIKE '%$keyword%' OR dau_admin.domain_person LIKE '%$keyword%' OR dau_koord.username LIKE '%$keyword%' OR dau_koord.nm_person LIKE '%$keyword%' OR dau_koord.domain_person LIKE '%$keyword%' OR dau_pelaksana.username LIKE '%$keyword%' OR dau_pelaksana.nm_person LIKE '%$keyword%' OR dau_pelaksana.domain_person LIKE '%$keyword%' OR olp.nm LIKE '%$keyword%' OR dau.username LIKE '%$keyword%' OR dau.nm_person LIKE '%$keyword%' OR dau.domain_person LIKE '%$keyword%' OR olk.nm_kampus LIKE '%$keyword%' OR olg.nm_gedung LIKE '%$keyword%' OR olr.nm_ruang LIKE '%$keyword%' OR oak.nm LIKE '%$keyword%' OR otk.nm LIKE '%$keyword%' OR oap.nm LIKE '%$keyword%' OR ov.nm LIKE '%$keyword%') AND r.status_tiket ='4' ORDER BY r.id DESC";
+          
+          $result = mysqli_query($con, $sql);
+          }else{
+          $reload = "aduanProsesPenanganan.php?pagination=true";
+          $sql = "SELECT * FROM tiket_masuk WHERE status_tiket ='4' ORDER BY id DESC";
+          $result = mysqli_query($con, $sql);
+          }
+          
+          $rpp = 20;
+          $page = isset($_GET["page"]) ? (intval($_GET["page"])) : 1;
+          $tcount = mysqli_num_rows($result);
+          $tpages = ($tcount) ? ceil($tcount/$rpp) : 1;
+          $count = 0;
+          $i = ($page-1)*$rpp;
+          $no_urut = ($page-1)*$rpp;
+          ?>
+        <section class="content">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-sm mb-2">
+                <form method="post" action="aduanProsesPenanganan.php">
+                  <?php include ("undefined_array_key.php");?>
+                  <div class="input-group">
+                    <input type="search" name="keyword" class="form-control form-control-sm" placeholder="Kata kunci pencarian..." value="<?php echo $_REQUEST['keyword'];?>" required>
+                    <div class="input-group-append">
+                      <button type="submit" class="btn btn-sm btn-default">
+                      <i class="fa fa-search"></i>
+                      </button>
+                      <?php
+                        if($_REQUEST['keyword']<>""){
+                        ?>
+                      <a class="btn btn-sm btn-warning" title="Refresh" href="aduanProsesPenanganan.php"><i class="fas fa-sync"></i> Refresh</a>
+                      <?php
+                        }
+                        ?>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div class="row">
+              <section class="col-sm-12 connectedSortable">
+                <div class="card card-outline card-info">
+                  <div class="card-header">
+                    <div class="clearfix">
+                      <h4 class="card-title float-left">Proses Penanganan</h4>
+                    </div>
+                  </div>
+                  <div class="card-body p-0">
+                    <div class="table-responsive-no-wrap">
+                      <table class="table table-hover m-0 table-sm custom">
+                        <thead class="thead-light">
+                          <th width="4%" class="pl-1">No.</th>
+                          <th width="10%">Status Approval</th>
+                          <th width="24%">Pengaju Aduan</th>
+                          <th width="12%">Tgl. Pengajuan</th>
+                          <th width="14%">Aitem Perbaikan</th>
+                          <th width="28%">Lokasi Perbaikan</th>
+                          <th width="4%">&nbsp;</th>
+                          <th width="4%" class="pr-1">&nbsp;</th>
+                        </thead>
+                        <tbody>
+                          <?php
+                            while(($count<$rpp) && ($i<$tcount)) {
+                            mysqli_data_seek($result, $i);
+                            $data = mysqli_fetch_array($result);
+                            
+                            $qpt = "SELECT * FROM dt_all_user WHERE username = '$data[nm_pengaju]'";
+                            $rpt = mysqli_query($con, $qpt);
+                            $dpt = mysqli_fetch_assoc($rpt);
+                            
+                            $queryNmLevel = "SELECT * FROM opsi_level_user WHERE id='$dpt[level]'";
+                            $rNmLevel = mysqli_query($con, $queryNmLevel);
+                            $dNmLevel = mysqli_fetch_assoc($rNmLevel);
+                            
+                            $qolp = "SELECT * FROM opsi_level_pengajuan WHERE id = '$data[level_pengajuan]'";
+                            $rolp = mysqli_query($con, $qolp);
+                            $dolp = mysqli_fetch_assoc($rolp);
+                            
+                            $qoak = "SELECT * FROM opsi_aitem_kerusakan WHERE id = '$data[aitem_kerusakan]'";
+                            $roak = mysqli_query($con, $qoak);
+                            $doak = mysqli_fetch_assoc($roak);
+                            
+                            $qotk = "SELECT * FROM opsi_tingkat_kerusakan WHERE id = '$data[tingkat_kerusakan]'";
+                            $rotk = mysqli_query($con, $qotk);
+                            $dotk = mysqli_fetch_assoc($rotk);
+                            
+                            $qolk = "SELECT * FROM opsi_lokasi_kampus WHERE id_kampus = '$data[lokasi_kampus]'";
+                            $rolk = mysqli_query($con, $qolk);
+                            $dolk = mysqli_fetch_assoc($rolk);
+                            
+                            $qolg = "SELECT * FROM opsi_lokasi_gedung WHERE id_gedung = '$data[lokasi_gedung]'";
+                            $rolg = mysqli_query($con, $qolg);
+                            $dolg = mysqli_fetch_assoc($rolg);
+                            
+                            $qolr = "SELECT * FROM opsi_lokasi_ruang WHERE id_ruang = '$data[lokasi_ruang]'";
+                            $rolr = mysqli_query($con, $qolr);
+                            $dolr = mysqli_fetch_assoc($rolr);
+                            
+                            $qost = "SELECT * FROM opsi_status_tiket WHERE id = '$data[status_tiket]'";
+                            $rost = mysqli_query($con, $qost);
+                            $dost = mysqli_fetch_assoc($rost);
+                            
+                            $qoap = "SELECT * FROM opsi_approval WHERE id = '$data[approval]'";
+                            $roap = mysqli_query($con, $qoap);
+                            $doap = mysqli_fetch_assoc($roap);
+
+                            $qov = "SELECT * FROM opsi_validasi WHERE id = '$data[validasi_admin]'";
+                            $rov = mysqli_query($con, $qov);
+                            $dov = mysqli_fetch_assoc($rov);
+
+                            $qdpt = "SELECT COUNT(id) AS jumKosong FROM dt_pelaksanaan_tiket WHERE id_tiket = '$data[id]' AND eviden = ''";
+                            $rdpt = mysqli_query($con, $qdpt);
+                            $ddpt = mysqli_fetch_assoc($rdpt);
+                            $jumKosong = $ddpt['jumKosong'];
+                            ?>
+                          <tr data-widget="expandable-table" aria-expanded="false">
+                            <td class="pl-1"><?php echo ++$no_urut;?></td>
+                            <td class="">
+                              <?php if($data['nm_pengaju']==$idAdm) {
+                              include "approvalKhususAdm.php";} else {
+                              echo '<code>'.$doap['nm'].'</code>';}?>
+                            </td>
+                            <td class=""><?php echo $dpt['nm_person'].' '.'['.$dpt['domain_person'].']' ;?></td>
+                            <td class=""><?php echo $data['tgl_pengajuan'];?></td>
+                            <td class=""><?php echo $doak['nm'];?></td>
+                            <td class=""><?php echo $dolr['nm_ruang'].' '.$dolg['nm_gedung'].' '.$dolk['nm_kampus'];?></td>
+                            <td class="">
+                              <button type="button" class="btn btn-warning btn-xs btn-block" data-toggle="modal" data-target="#fotoKerusakan" data-whatever="<?php echo $data['id'];?>" title="Lihat foto kerusakan"><i class="far fa-image"></i></button>
+                              <div class="modal fade" id="fotoKerusakan" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="fotoKerusakanLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                  <div class="modal-content">
+                                    <div class="modal-header">
+                                      <h5 class="modal-title" id="fotoKerusakan">Foto Kerusakan</h5>
+                                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                      <span aria-hidden="true">&times;</span>
+                                      </button>
+                                    </div>
+                                    <div class="modal-body">
+                                      <div class="isiFotoKerusakan"></div>
+                                    </div>
+                                    <div class="modal-footer">
+                                      <button type="button" class="btn btn-info btn-sm" data-dismiss="modal">Close</button>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </td>
+                            <td class="pr-1">
+                              <button type="button" class="btn btn-success btn-xs btn-block" data-toggle="modal" data-target="#fotoDtDukung" data-whatever="<?php echo $data['id'];?>" title="Lihat foto bukti penanganan aduan"><i class="fas fa-photo-video"></i></button>
+                              <div class="modal fade" id="fotoDtDukung" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="fotoDtDukungLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                  <div class="modal-content">
+                                    <div class="modal-header">
+                                      <h5 class="modal-title" id="fotoDtDukung">Foto Bukti Penanganan Aduan</h5>
+                                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                      <span aria-hidden="true">&times;</span>
+                                      </button>
+                                    </div>
+                                    <div class="modal-body">
+                                      <div class="isiFotoDtDukung"></div>
+                                    </div>
+                                    <div class="modal-footer">
+                                      <button type="button" class="btn btn-info btn-sm" data-dismiss="modal">Close</button>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </td>
+                          </tr>
+                          <tr class="expandable-body">
+                            <td colspan="8">
+                              <div class="card bg-gradient-light rounded-0 shadow">
+                                <div class="card-body p-0">
+                                  <?php include("ketTiket.php");?>
+                                </div>
+                              </div>
+                            </td>
+                          </tr>
+                          <?php
+                            $i++; 
+                            $count++;
+                            }
+                            ?>                        
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                  <div class="card-footer pb-0 clearfix">
+                    <div class="float-right"><?php echo paginate_one($reload, $page, $tpages);?></div>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+    <?php include( "footerUser.php" );?>
+    <?php include( "jsUser.php" );?>
+  </body>
+</html>
---
+++ aitemKeluarPerBulanKu.php
@@ -0,0 +1,157 @@
+<?php include( "contentsConUser.php" );
+  $username = $_SESSION['username'];
+  $tahun = mysqli_real_escape_string($con,  $_GET[ 'tahun' ] );
+  $page = mysqli_real_escape_string($con,  $_GET[ 'page' ] );
+
+  function getBulanIndonesia($bulanAngka) {
+    $bulan = [1 => 'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
+    $index = (int)$bulanAngka; 
+    return $bulan[$index] ?? 'Bulan tidak valid';}
+
+  $sql = "SELECT MONTH(thn_pengambilan) as bulan, COUNT(*) as jumData FROM kebutuhan_material_tiket WHERE YEAR(thn_pengambilan)='$tahun' GROUP BY MONTH(thn_pengambilan) ORDER BY bulan DESC";
+    $result = mysqli_query($con, $sql);
+    $chart_data="";
+    WHILE($row = mysqli_fetch_array($result)) {
+      $xBulan[]  = getBulanIndonesia($row['bulan']);
+      $yJumlah[] = $row['jumData'];
+  }
+  ?>
+<!DOCTYPE html>
+<html lang="en">
+  <?php include( "headUser.php" );?> 
+  <body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper" style="min-height: 223.667px;">
+      <?php 
+        include( "navtopUser.php" );
+        include( "navKu.php" );
+        ?> 
+      <div class="content-wrapper">
+        <div class="content-header">
+          <div class="container-fluid">
+            <?php include( "alertUser.php" );?>
+            <div class="row mb-2">
+              <div class="col-sm-4">
+                <h4 class="mb-0">Rekap Data</h4>
+              </div>
+              <div class="col-sm-8">
+                <ol class="breadcrumb float-sm-right">
+                  <li class="breadcrumb-item"><a href="rekapGudangKu.php">Persediaan Barang</a></li>
+                  <li class="breadcrumb-item"><a href="aitemKeluarPertahunKu.php?page=<?php echo $page;?>">Pengeluaran</a></li>
+                  <li class="breadcrumb-item active">Tahun <?php echo $tahun;?></li>
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+        <section class="content">
+          <div class="container-fluid">
+            <div class="row">
+              <section class="col-lg-6 col-md-6 col-sm-12 connectedSortable">
+                <?php include 'pagination1.php';
+                  $reload1 = "aitemKeluarPerBulanKu.php?tahun=$tahun&page=$page&pagination=true";
+                  $sql1 = "SELECT MONTH(thn_pengambilan) as bulan, COUNT(*) as jumData FROM kebutuhan_material_tiket WHERE YEAR(thn_pengambilan)='$tahun' GROUP BY MONTH(thn_pengambilan) ORDER BY bulan DESC";
+                  $result1 = mysqli_query($con, $sql1);
+              
+                  $rpp1 = 12;
+                  $page1 = isset($_GET["page1"]) ? (intval($_GET["page1"])) : 1;
+                  $tcount1 = mysqli_num_rows($result1);
+                  $tpages1 = ($tcount1) ? ceil($tcount1/$rpp1) : 1;
+                  $count1 = 0;
+                  $i1 = ($page1-1)*$rpp1;
+                  $no_urut1 = ($page1-1)*$rpp1;
+                ?>
+                <div class="card card-outline card-info">
+                  <div class="card-header">
+                    <div class="clearfix">
+                      <h4 class="card-title float-left">Tahun <?php echo $tahun;?></h4>
+                    </div>
+                  </div>
+                  <div class="card-body p-0">
+                    <div class="table-responsive-no-wrap">
+                      <table class="table table-hover m-0 table-sm custom">
+                        <thead class="thead-light">
+                          <th width="4%" class="pl-1">No.</th>
+                          <th width="36%">Bulan</th>
+                          <th width="56%">Frekuensi Pengeluaran</th>
+                          <th class="pr-1" width="4%">Opsi</th>
+                        </thead>
+                        <tbody>
+                          <?php
+                            while(($count1<$rpp1) && ($i1<$tcount1)) {
+                            mysqli_data_seek($result1, $i1);
+                            $data = mysqli_fetch_array($result1);
+                            ?>
+                          <tr>
+                            <td class="pl-1"><?php echo ++$no_urut1;?></td>
+                            <td class=""><?php echo getBulanIndonesia($data['bulan']);?></td>
+                            <td class=""><?php echo $data['jumData'];?></td>
+                            <td class="pr-1"><a class="btn btn-info btn-xs btn-block" href="aitemKeluarPerTglKu.php?tahun=<?php echo $tahun;?>&bulan=<?php echo $data['bulan'];?>&page=<?php echo $page;?>&page1=<?php echo $page1;?>" title="Lihat Detail"><i class="fas fa-angle-double-right"></i> Lihat Detail</a></td>
+                          </tr>
+                          <?php
+                            $i1++; 
+                            $count1++;
+                            }
+                            ?>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                  <div class="card-footer pb-0 clearfix">
+                    <div class="float-right"><?php echo paginate_one1($reload1, $page1, $tpages1);?></div>
+                  </div>
+                </div>
+              </section>
+              <section class="col-lg-6 col-md-6 col-sm-12 connectedSortable">
+                <div class="card card-outline card-info">
+                  <div class="card-header">
+                    <div class="clearfix">
+                      <h4 class="card-title float-left">Grafik Tahun <?php echo $tahun;?></h4>
+                    </div>
+                  </div>
+                  <div class="card-body pt-0">
+                    <canvas id="tekBul"></canvas>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+    <?php include( "footerUser.php" );?>
+    <?php include( "jsUser.php" );?>
+    <script>
+      var barColors = ["rgba(255, 99, 132, 0.4)","rgba(54, 162, 235, 0.4)","rgba(255, 206, 86, 0.4)","rgba(75, 192, 192, 0.4)","rgba(255, 255, 0, 0.4)","rgba(64, 224, 208, 0.4)","rgba(0, 128, 128, 0.4)","rgba(70, 130, 180, 0.4)","rgba(106, 90, 205, 0.4)","rgba( 160, 82, 45, 0.4)","rgba(46, 139, 87, 0.4)","rgba(65, 105, 225, 0.4)","rgba(255, 0, 0, 0.4)","rgba(128, 0, 128, 0.4)","rgba( 255, 165, 0, 0.4)","rgba(199, 21, 133, 0.4)"];
+      
+      new Chart("tekBul", {
+      type: "bar",
+      data: {
+      labels: <?php echo json_encode($xBulan);?>,
+      datasets: [{
+      backgroundColor: barColors,
+      data: <?php echo json_encode($yJumlah);?>
+      }]
+      },
+      options: {
+      scales: {
+         yAxes: [{
+             ticks: {
+                 beginAtZero: true,
+                 userCallback: function(label, index, labels) {
+                     if (Math.floor(label) === label) {
+                         return label;
+                     }
+      
+                 },
+             }
+         }],
+      },
+        legend: {display: false},
+      title: {
+      display: true
+      }
+      }
+      });
+    </script>
+  </body>
+</html>
